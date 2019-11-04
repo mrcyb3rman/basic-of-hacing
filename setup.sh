4bbb7c9688aeea275909408f97db78ee1085647b
mrcyb3rman <57321604+mrcyb3rman@users.noreply.github.com>
2019-11-04 00:49:05 -0500

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+clear
+echo
+echo "[!] Updating..."
+apt update -y > install.log
+echo
+echo "[!] Installing Dependencies..."
+echo
+echo '    [+] dnsutils'
+apt-get install dnsutils&>> install.log
+echo "    [+] nmap"
+apt-get install nmap&>> install.log
+echo "    [+] theharvester"
+apt-get install theharvester&>> install.log
+echo "    [+] metagoofil"
+apt-get install metagoofil&>> install.log
+echo
+echo "[!] Setting Permissions..."
+chmod 777 0n3.sh
+echo -e "\n[+] You are Good to go!\n"
