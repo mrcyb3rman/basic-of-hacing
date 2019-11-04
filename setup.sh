c08ad7db3d5718e0650deceba4f25fe3996a8cf8
mrcyb3rman <57321604+mrcyb3rman@users.noreply.github.com>
2019-11-04 00:20:33 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 clear
-apt update -y
-apt install dnsutils nmap theharvester metagoofil -y
+echo
+echo "[!] Updating..."
+apt update -y > install.log
+echo
+echo "[!] Installing Dependencies..."
+echo '    [+] dnsutils'
+apt install dnsutils & >> install.log
+echo "    [+] nmap"
+apt install nmap & >> install.log
+echo "    [+] theharvester"
+apt install theharvester & >> install.log
+echo "    [+] metagoofil"
+apt install metagoofil & >> install.log
+echo
+echo "[!] Setting Permissions..."
+chmod 777 allinone.sh
 echo -e "\n[+] You are Good to go!\n"
